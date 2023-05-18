d54537aa8fc469a8115fbe8e1cefb70e4c6080f9
avexyy <113837032+avexyy@users.noreply.github.com>
2023-05-18 12:32:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿if (args.Length == 3)
+{
+    string path = args[0];
+    string file = args[1];
+    string text = args[2];
+    bool tmp = true;
+    int numberOfFiles = 0;
+    int numberOfLines = 0;
+    int numberOfOccurences = 0;
+
+    if (!Directory.Exists(path))
+    {
+        Console.WriteLine("Invalid path: " + path);
+        return;
+    }
+
+    var files = Directory.GetFiles(path, "*.txt");
+
+    int counter = 0;
+
+    foreach (var filePath in files)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+        numberOfFiles++;
+        foreach (var line in lines)
+        {
+            counter++;
+            {
+                if (line.Contains(text))
+                {
+                    if (tmp) { Console.WriteLine($"{filePath}"); tmp = false; }
+                    Console.WriteLine($"{counter}: {line}");
+                    numberOfLines++;
+                }
+            }
+        }
+        counter = 0;
+        tmp = true;
+    }
+    System.Console.WriteLine("SUMMARY:");
+    System.Console.WriteLine($"Number of found files: {numberOfFiles} ");
+    System.Console.WriteLine($"Number of found lines: {numberOfLines} ");
+    System.Console.WriteLine($"Number of occurences: {numberOfOccurences}");
+}
+else
+{
+    System.Console.WriteLine("Invalid arguments!");
+}
